8fe75134cf822a5c8ac14b33ad48cd53f032b9b8
ji213 <ittyjonathan@gmail.com>
2026-02-18 18:09:13 -0500

    Create table definitions initialize dummy data

---
+++ SQLScripts/TableScripts/dbo.tbl_patient.table.sql
@@ -0,0 +1,28 @@
+-- Check if the table exists in the 'dbo' schema
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[tbl_patient]') AND type in (N'U'))
+BEGIN
+    CREATE TABLE dbo.tbl_patient (
+        PatientID INT PRIMARY KEY IDENTITY(1,1),
+        FirstName NVARCHAR(50) NOT NULL,
+        LastName NVARCHAR(50) NOT NULL,
+        SSN NVARCHAR(11) NOT NULL, -- Format: XXX-XX-XXXX
+        DateOfBirth DATE NOT NULL,
+        Gender NVARCHAR(10),
+        Email NVARCHAR(100),
+        PhoneNumber NVARCHAR(20),
+        AddressLine1 NVARCHAR(255),
+        City NVARCHAR(100),
+        State NVARCHAR(50),
+        ZipCode NVARCHAR(20),
+        InsuranceProvider NVARCHAR(100),
+        IsActive BIT DEFAULT 1,
+        CreatedDt DATETIME DEFAULT GETDATE(),
+        UpdatedDt DATETIME 
+    );
+    
+    PRINT 'Table [dbo].[tbl_patient] created successfully.';
+END
+ELSE
+BEGIN
+    PRINT 'Table [dbo].[tbl_patient] already exists. Skipping creation.';
+END
