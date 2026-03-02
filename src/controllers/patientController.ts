f0a8a75ac836ce0b91760061b671236717521515
ji213 <ittyjonathan@gmail.com>
2026-03-01 19:22:36 -0500

    Generate random PublicPatientID to be used for DELETE and PATCH

--- src/controllers/patientController.ts
+++ src/controllers/patientController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express'
 import { getConnection } from '../config/dbConfig.js'
+import { generatePublicPatientID } from '../utils/idGenerator.js';
 import { formatName, nameRegex, ssnRegex, dateRegex, ALLOWED_GENDERS, emailRegex, phoneRegex, zipRegex } from '../utils/stringUtils.js';
 
 const US_STATES = new Set([
@@ -192,276 +193,301 @@ export const getPatientByID = async (req: Request, res: Response) => {
 };
 
 export const postPatient = async (req: Request, res: Response) => {
-    try{
-        const pool = await getConnection();
-        const request = pool.request();
+    // Initialize variables
+    let isInserted = false;
+    let retryCount = 0;
 
-        // Extract all fields
-        let {
-            FirstName, LastName, SSN, DateOfBirth,
-            Gender, Email, PhoneNumber, AddressLine1,
-            City, State, ZipCode, InsuranceProvider
-        } = req.body;
+    const maxRetries = 5;
 
-        // Validate Minimum Required Fields
-        if (!FirstName || !LastName || !SSN || !DateOfBirth){
-            return res.status(400).json({
-                status: 400,
-                message: "Missing required fields: FirstName, LastName, SSN, and DOB are mandatory",
-                error: "POST_REQUIRED_FIELDS_MISSING"
-            });
-        }
+    while(!isInserted && retryCount < maxRetries){
+        let publicID = generatePublicPatientID();
 
-        // Validate First and Last Name
+        try{
 
-        const sanitizedFirstName = formatName(FirstName);
-        const sanitizedLastName = formatName(LastName);
+            const pool = await getConnection();
+            const request = pool.request();
 
-        if (sanitizedFirstName.length < 2 || sanitizedLastName.length < 2){
-            return res.status(400).json({
-                status: 400,
-                message: "Names must be at least 2 characters long",
-                error: "POST_NAME_LENGTH_ERROR"
-            });
-        }
-        
-        if(!nameRegex.test(sanitizedFirstName) || !nameRegex.test(sanitizedLastName)){
-            return res.status(400).json({
-                status: 400,
-                message: "Names contain invalid characters",
-                error: "POST_NAME_INVALID_CHAR"
-            });
-        }
-        // Validate SSN Format
-        
-        if(!ssnRegex.test(SSN)){
-            return res.status(400).json({
-                status: 400,
-                message: "Invalid SSN format. Expected XXX-XX-XXXX",
-                error: "POST_INVALID_SSN_FORMAT"
-            });
-        }
+            // Extract all fields
+            let {
+                FirstName, LastName, SSN, DateOfBirth,
+                Gender, Email, PhoneNumber, AddressLine1,
+                City, State, ZipCode, InsuranceProvider
+            } = req.body;
 
-
-        // Validate DateOfBirth
-        // Check format
-        if (!dateRegex.test(DateOfBirth)){
-            return res.status(400).json({
-                status: 400,
-                message: "DOB must be in YYYY-MM-DD format",
-                error: "POST_INVALID_DOB_FORMAT"
-            });
-        }
-
-        const dob = new Date(DateOfBirth);
-
-        // check if it is a real date
-        if(isNaN(dob.getTime())){
-            return res.status(400).json({
-                status: 400,
-                message: "Invalid Date value",
-                error: "POST_INVALID_DOB_VALUE"
-            });
-        }
-
-        const today = new Date();
-        if (dob > today){
-            return res.status(400).json({
-                status: 400,
-                message: "DOB cannot be in future",
-                error: "POST_DOB_OUTOFBOUNDS"
-            });
-        }
-
-        // establish insert string
-        // dont close brackets yet, close at end
-        let insertString = `INSERT INTO dbo.tbl_patient ( 
-        FirstName
-        , LastName
-        , SSN 
-        , DateOfBirth `;
-        // establish values string
-        let valuesString = `VALUES ( @FirstName, @LastName, @SSN, @DateOfBirth`;
-
-        request.input('FirstName', sanitizedFirstName);
-        request.input('LastName', sanitizedLastName);
-        request.input('SSN', SSN);
-        request.input('DateOfBirth', DateOfBirth);
-
-
-        if(Gender){
-            // Validate Gender Format
-            Gender = Gender.toUpperCase().trim();
-
-            if(!ALLOWED_GENDERS.includes(Gender)){
+            // Validate Minimum Required Fields
+            if (!FirstName || !LastName || !SSN || !DateOfBirth){
                 return res.status(400).json({
                     status: 400,
-                    message: "Invalid Gender supplied, please supply M or F",
-                    error: "POST_INVALID_GENDER"
+                    message: "Missing required fields: FirstName, LastName, SSN, and DOB are mandatory",
+                    error: "POST_REQUIRED_FIELDS_MISSING"
                 });
             }
 
-            // Add to strings
-            insertString += `, Gender`;
-            valuesString += `, @Gender`;
+            // Validate First and Last Name
 
-            request.input('Gender', Gender);
+            const sanitizedFirstName = formatName(FirstName);
+            const sanitizedLastName = formatName(LastName);
 
-        }
-        
-        if(Email){
-            // Validate Email Format
-            Email = Email.trim().toLowerCase();
-
-            if(!emailRegex.test(Email)){
+            if (sanitizedFirstName.length < 2 || sanitizedLastName.length < 2){
                 return res.status(400).json({
                     status: 400,
-                    message: "Invalid Email Format",
-                    error: "POST_INVALID_EMAIL_FORMAT"
+                    message: "Names must be at least 2 characters long",
+                    error: "POST_NAME_LENGTH_ERROR"
                 });
             }
 
-            // Add to Strings
-            insertString += `, Email`;
-            valuesString += `, @Email`;
-
-            request.input('Email', Email);
-        }
-
-        if(PhoneNumber){
-            // Validate Phone Number
-            // Remove everything that is not a number
-
-            const digitsOnly = PhoneNumber.replace(/\D/g, '');
-
-            if(!phoneRegex.test(digitsOnly)){
+            if(!nameRegex.test(sanitizedFirstName) || !nameRegex.test(sanitizedLastName)){
                 return res.status(400).json({
                     status: 400,
-                    message: "Phone number must be exactly 10 digits.",
-                    error: "INVALID_PHONE_FORMAT"
+                    message: "Names contain invalid characters",
+                    error: "POST_NAME_INVALID_CHAR"
+                });
+            }
+            // Validate SSN Format
+
+            if(!ssnRegex.test(SSN)){
+                return res.status(400).json({
+                    status: 400,
+                    message: "Invalid SSN format. Expected XXX-XX-XXXX",
+                    error: "POST_INVALID_SSN_FORMAT"
                 });
             }
 
-            PhoneNumber = digitsOnly;
+
+            // Validate DateOfBirth
+            // Check format
+            if (!dateRegex.test(DateOfBirth)){
+                return res.status(400).json({
+                    status: 400,
+                    message: "DOB must be in YYYY-MM-DD format",
+                    error: "POST_INVALID_DOB_FORMAT"
+                });
+            }
+
+            const dob = new Date(DateOfBirth);
+
+            // check if it is a real date
+            if(isNaN(dob.getTime())){
+                return res.status(400).json({
+                    status: 400,
+                    message: "Invalid Date value",
+                    error: "POST_INVALID_DOB_VALUE"
+                });
+            }
+
+            const today = new Date();
+            if (dob > today){
+                return res.status(400).json({
+                    status: 400,
+                    message: "DOB cannot be in future",
+                    error: "POST_DOB_OUTOFBOUNDS"
+                });
+            }
+
+            // establish insert string
+            // dont close brackets yet, close at end
+            let insertString = `INSERT INTO dbo.tbl_patient ( 
+            FirstName
+            , LastName
+            , SSN 
+            , DateOfBirth
+            , PublicPatientID `;
+            // establish values string
+            let valuesString = `VALUES ( @FirstName, @LastName, @SSN, @DateOfBirth, @PublicPatientID`;
+
+            request.input('FirstName', sanitizedFirstName);
+            request.input('LastName', sanitizedLastName);
+            request.input('SSN', SSN);
+            request.input('DateOfBirth', DateOfBirth);
+            request.input('PublicPatientID', publicID);
 
 
-            // Add to Strings
-            insertString += `, PhoneNumber`;
-            valuesString += `, @PhoneNumber`;
+            if(Gender){
+                // Validate Gender Format
+                Gender = Gender.toUpperCase().trim();
 
-            request.input('PhoneNumber', PhoneNumber);
-        }
+                if(!ALLOWED_GENDERS.includes(Gender)){
+                    return res.status(400).json({
+                        status: 400,
+                        message: "Invalid Gender supplied, please supply M or F",
+                        error: "POST_INVALID_GENDER"
+                    });
+                }
+
+                // Add to strings
+                insertString += `, Gender`;
+                valuesString += `, @Gender`;
+
+                request.input('Gender', Gender);
+
+            }
+        
+            if(Email){
+                // Validate Email Format
+                Email = Email.trim().toLowerCase();
+
+                if(!emailRegex.test(Email)){
+                    return res.status(400).json({
+                        status: 400,
+                        message: "Invalid Email Format",
+                        error: "POST_INVALID_EMAIL_FORMAT"
+                    });
+                }
+
+                // Add to Strings
+                insertString += `, Email`;
+                valuesString += `, @Email`;
+
+                request.input('Email', Email);
+            }
+
+            if(PhoneNumber){
+                // Validate Phone Number
+                // Remove everything that is not a number
+
+                const digitsOnly = PhoneNumber.replace(/\D/g, '');
+
+                if(!phoneRegex.test(digitsOnly)){
+                    return res.status(400).json({
+                        status: 400,
+                        message: "Phone number must be exactly 10 digits.",
+                        error: "INVALID_PHONE_FORMAT"
+                    });
+                }
+
+                PhoneNumber = digitsOnly;
+
+
+                // Add to Strings
+                insertString += `, PhoneNumber`;
+                valuesString += `, @PhoneNumber`;
+
+                request.input('PhoneNumber', PhoneNumber);
+            }
         
         
 
-        // For address, we need all 4 variables supplied to post the data, 
-        // if any of the address fields are missing if one of them are supplied, raise error
+            // For address, we need all 4 variables supplied to post the data, 
+            // if any of the address fields are missing if one of them are supplied, raise error
 
-        if (AddressLine1 && City && State && ZipCode ){
-            // Validate AddressLine1
-            AddressLine1 = AddressLine1.trim();
-            // Validate City
-            City = City.trim();
+            if (AddressLine1 && City && State && ZipCode ){
+                // Validate AddressLine1
+                AddressLine1 = AddressLine1.trim();
+                // Validate City
+                City = City.trim();
 
-            // Validate State
-            State = State.trim().toUpperCase();
+                // Validate State
+                State = State.trim().toUpperCase();
 
-            if(!US_STATES.has(State)){
-                return res.status(400).json({ 
+                if(!US_STATES.has(State)){
+                    return res.status(400).json({ 
+                        status: 400,
+                        message: "Invalid State abbreviation. Please provide a valid US state.",
+                        error: "POST_INVALID_STATE"
+                    });
+                }
+                // Validate ZipCode
+                ZipCode = ZipCode.trim();
+                if(!zipRegex.test(ZipCode)){
+                    return res.status(400).json({ 
+                        status: 400,
+                        message: "Zip Code must be exactly 5 digits.",
+                        error: "POST_INVALID_ZIP"
+                    });
+                }
+
+                // Add to Strings
+                insertString += `, AddressLine1
+                , City
+                , State
+                , ZipCode `;
+
+                valuesString += `, @AddressLine1, @City, @State, @ZipCode`;
+
+                request.input('AddressLine1', AddressLine1);
+                request.input('City', City);
+                request.input('State', State);
+                request.input('ZipCode', ZipCode);
+
+            } else if (AddressLine1 || City || State || ZipCode){
+                // Only partial fields provided.... raise error
+                return res.status(400).json({
                     status: 400,
-                    message: "Invalid State abbreviation. Please provide a valid US state.",
-                    error: "POST_INVALID_STATE"
+                    message: "All address fields required if any address fields are provided",
+                    error: "POST_PARTIAL_ADDRESS_SUPPLIED"
                 });
             }
-            // Validate ZipCode
-            ZipCode = ZipCode.trim();
-            if(!zipRegex.test(ZipCode)){
-                return res.status(400).json({ 
-                    status: 400,
-                    message: "Zip Code must be exactly 5 digits.",
-                    error: "POST_INVALID_ZIP"
-                });
+        
+
+            if(InsuranceProvider){
+                // Validate InsuranceProvider
+                InsuranceProvider = InsuranceProvider.trim();
+
+                if(InsuranceProvider.length === 0){
+                    return res.status(400).json({
+                        status: 400,
+                        message: "Insurance Provider cannot be empty if provided",
+                        error: "POST_EMPTY_INSURANCE"
+                    });
+                }
+
+                if(InsuranceProvider.length> 100){
+                    return res.status(400).json({
+                        status: 400,
+                        message: "Insurance Provider name is too long (max 100 char)",
+                        error: "POST_INVALID_INSURANCE"
+                    });
+                }
+
+
+
+                // Add to Strings
+                insertString += `, InsuranceProvider`;
+                valuesString += `, @InsuranceProvider`;
+
+                request.input('InsuranceProvider', InsuranceProvider);
             }
+        
+        
 
-            // Add to Strings
-            insertString += `, AddressLine1
-            , City
-            , State
-            , ZipCode `;
 
-            valuesString += `, @AddressLine1, @City, @State, @ZipCode`;
+            // After all validations, close brackets
+            insertString += `)`;
+            valuesString += `)`;
 
-            request.input('AddressLine1', AddressLine1);
-            request.input('City', City);
-            request.input('State', State);
-            request.input('ZipCode', ZipCode);
+            let finalQuery = insertString + valuesString;
 
-        } else if (AddressLine1 || City || State || ZipCode){
-            // Only partial fields provided.... raise error
-            return res.status(400).json({
-                status: 400,
-                message: "All address fields required if any address fields are provided",
-                error: "POST_PARTIAL_ADDRESS_SUPPLIED"
+            //console.log(` SQL Query: ${finalQuery}`);
+
+            // Insert logic into database
+            await request.query(finalQuery);
+
+            isInserted = true;
+
+            res.status(201).json({
+                status: 201,
+                message: "Patient created successfully"
             });
-        }
-        
 
-        if(InsuranceProvider){
-            // Validate InsuranceProvider
-            InsuranceProvider = InsuranceProvider.trim();
 
-            if(InsuranceProvider.length === 0){
-                return res.status(400).json({
-                    status: 400,
-                    message: "Insurance Provider cannot be empty if provided",
-                    error: "POST_EMPTY_INSURANCE"
-                });
+        } catch(err: any){
+
+            // Error Handling for "Unique Constraint Violation" (SQL Error 2627)
+            if(err.number === 2627){
+                console.warn(`Collision detected on PublicPatientID ${publicID}. Retrying...`);
             }
-
-            if(InsuranceProvider.length> 100){
-                return res.status(400).json({
-                    status: 400,
-                    message: "Insurance Provider name is too long (max 100 char)",
-                    error: "POST_INVALID_INSURANCE"
+            else{
+                console.error("POST ERROR: ", err);
+                res.status(500).json({
+                    status: 500,
+                    message: "Internal Server Error",
+                    error: "FATAL_POST_ERROR"
                 });
+
             }
-
-
-
-            // Add to Strings
-            insertString += `, InsuranceProvider`;
-            valuesString += `, @InsuranceProvider`;
-
-            request.input('InsuranceProvider', InsuranceProvider);
+            
         }
-        
-        
 
-
-        // After all validations, close brackets
-        insertString += `)`;
-        valuesString += `)`;
-
-        let finalQuery = insertString + valuesString;
-
-        //console.log(` SQL Query: ${finalQuery}`);
-
-        // Insert logic into database
-        await request.query(finalQuery);
-
-        res.status(201).json({
-            status: 201,
-            message: "Patient created successfully"
-        });
-        
-
-    } catch(err){
-        console.error("POST ERROR: ", err);
-        res.status(500).json({
-            status: 500,
-            message: "Internal Server Error",
-            error: "FATAL_POST_ERROR"
-        });
     }
 
 };
